50519634e80b1a8da33f0d1da4aace3f08b453c6
Igor Marchi <igoraugustomarchi@gmail.com>
2023-10-04 22:23:03 -0300

    feat: add core app

---
+++ src/infra/http/controllers/create-account.controller.e2e-spec.ts
@@ -0,0 +1,41 @@
+import { AppModule } from '@/infra/app.module';
+import { PrismaService } from '@/infra/prisma/prisma.service';
+import { INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import request from 'supertest';
+
+describe('Create account (E2E)', () => {
+  let app: INestApplication;
+  let prisma: PrismaService;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    prisma = moduleRef.get(PrismaService);
+
+    await app.init();
+  });
+
+  test('[POST] /account', async () => {
+    const email = 'johndoe@gmail.com';
+
+    const response = await request(app.getHttpServer()).post('/account').send({
+      name: 'John Doe',
+      email,
+      password: '123456',
+    });
+
+    expect(response.statusCode).toBe(201);
+
+    const userOnDataBase = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    expect(userOnDataBase).toBeTruthy();
+  });
+});
---
+++ src/infra/http/controllers/create-account.controller.ts
@@ -0,0 +1,54 @@
+import {
+  ConflictException,
+  Body,
+  Controller,
+  Post,
+  HttpCode,
+  UsePipes,
+} from '@nestjs/common';
+import { hash } from 'bcryptjs';
+import { z } from 'zod';
+import { PrismaService } from '@/infra/prisma/prisma.service';
+import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe';
+
+const createAccountBodySchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string(),
+});
+
+type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>;
+
+@Controller('/account')
+export class CreateAccountController {
+  constructor(private prisma: PrismaService) {}
+
+  @Post()
+  @HttpCode(201)
+  @UsePipes(new ZodValidationPipe(createAccountBodySchema))
+  async handle(@Body() body: CreateAccountBodySchema) {
+    const { name, email, password } = body;
+
+    const userWithSameEmail = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (userWithSameEmail) {
+      throw new ConflictException(
+        'User with same e-mail address already exists',
+      );
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+  }
+}
