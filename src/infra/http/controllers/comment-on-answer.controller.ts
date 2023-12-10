764869ed68f70f161f6596f6eb7b86a59eefdd2b
Igor Marchi <igoraugustomarchi@gmail.com>
2023-12-09 22:51:06 -0300

    feat: comment on answer controller

---
+++ src/infra/http/controllers/comment-on-answer.controller.ts
@@ -0,0 +1,44 @@
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Param,
+  Post,
+} from '@nestjs/common';
+import { z } from 'zod';
+import { CurrentUser } from '@/infra/auth/current-user-decorator';
+import { UserPayload } from '@/infra/auth/jwt.strategy';
+import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe';
+import { CommentOnAnswerUseCase } from '@/domain/forum/application/use-cases/comment-on-answer';
+
+const commentOnAnswerBodySchema = z.object({
+  content: z.string(),
+});
+
+type CommentOnAnswerBodySchema = z.infer<typeof commentOnAnswerBodySchema>;
+const bodyValidationPipe = new ZodValidationPipe(commentOnAnswerBodySchema);
+
+@Controller('/answer/:answerId/comment')
+export class CommentOnAnswerController {
+  constructor(private commentOnAnswer: CommentOnAnswerUseCase) {}
+
+  @Post()
+  async handle(
+    @Body(bodyValidationPipe) body: CommentOnAnswerBodySchema,
+    @CurrentUser() user: UserPayload,
+    @Param('answerId') answerId: string,
+  ) {
+    const { content } = body;
+    const { sub: userId } = user;
+
+    const result = await this.commentOnAnswer.execute({
+      content,
+      answerId,
+      authorId: userId,
+    });
+
+    if (result.isLeft()) {
+      throw new BadRequestException();
+    }
+  }
+}
